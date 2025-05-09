d1261529c013295013a420955fa08f24dcf5aa86
trws2 <dchen@cs.umd.edu>
2025-05-08 22:39:09 -0700

    update

--- src/MLC/binary_classification_with_logistic_regression.py
+++ src/MLC/binary_classification_with_logistic_regression.py
@@ -1,11 +1,15 @@
+
+
 import numpy as np
 
 def predict_logistic(X: np.ndarray, weights: np.ndarray, bias: float) -> np.ndarray:
-    tmp = np.dot(X, weights) + bias
-    p = 1 / (1 + np.exp(-tmp))
-    tmp = p >= 0.5 
-    tmp = tmp.astype(int)
-    return tmp.tolist()
+    y = np.dot(X, weights) + bias
+    prob = 1 / (1 + np.exp(-y))
+    pred = prob >= 0.5
+    pred = pred.astype(int)
+    pred = pred.tolist()
+    return pred
+
 
 print(predict_logistic(np.array([[0, 0], [0.1, 0.1], [-0.1, -0.1]]), np.array([1, 1]), 0))
 
--- src/MLC/implement_logistic_regression_using_gradient_descent.py
+++ src/MLC/implement_logistic_regression_using_gradient_descent.py
@@ -1,60 +1,110 @@
+# reference: https://www.geeksforgeeks.org/ml-logistic-regression-using-python/
+
 import numpy as np
+import pandas as pd
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 
+# Load the diabetes dataset
+diabetes = load_diabetes()
+X, y = diabetes.data, diabetes.target
+num_examples, feature_dim = X.shape
+
+# Convert the target variable to binary (1 for diabetes, 0 for no diabetes)
+y_binary = (y > np.median(y)).astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y_binary, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+X_train = np.append(X_train, np.ones((X_train.shape[0], 1)), axis=1)
+X_test = np.append(X_test, np.ones((X_test.shape[0], 1)), axis=1)
+
+# np.random.seed(42)
+# initialize logistic regression model wneights
+# include bias at the end
 np.random.seed(42)
+weights = np.random.rand(feature_dim+1, 1)
 
-# generate 2D data for two classes
+def sigmoid(logits):
+    return 1 / (1 + np.exp(-logits))
 
-mean1 = [1, 1]
-var1 = [[1, 0], [0, 1]]
-x_class1 = np.random.multivariate_normal(mean1, var1, size=10000)
-ones_column = np.ones((x_class1.shape[0], 1))
-x_class1 = np.column_stack((x_class1, ones_column))
+def compute_loss_and_gradient(X, y_binary, weights):
+
+    logits = np.dot(X, weights)
+    prob = sigmoid(logits)
+    num_examples = len(prob)
+    loss = np.sum(y_binary * np.log(prob) + (1 - y_binary) * np.log(1 - prob)) / -num_examples 
+
+    class1_X = X[y_binary == 1, :]
+    class0_X = X[y_binary == 0, :]
+    class1_prob = prob[y_binary == 1]
+    class0_prob = prob[y_binary == 0]
+
+    tmp = class0_prob * class0_X
+    part1 = (1 / num_examples) * np.sum(tmp, axis=0)
+
+    tmp = (1 - class1_prob) * class1_X
+    part2 = (-1 / num_examples) * np.sum(tmp, axis=0)
+
+    grad = part1 + part2
+
+    grad = grad.reshape(len(grad), 1)
+
+    return (loss, grad)
+
+def predict(X, y, weights):
+    class1_prob = sigmoid(np.dot(X, weights))
+    pred = class1_prob >= 0.5
+    pred = pred.reshape(1, pred.shape[0])
+    pred = pred.astype(int).tolist()[0]
+    acc = sum(pred == y) / len(pred)
+    print(f"acc={acc*100}")
 
 
-mean2 = [-1, -1]
-var2 = [[1, 0], [0, 1]]
-x_class2 = np.random.multivariate_normal(mean2, var2, size=10000)
-ones_column = np.ones((x_class2.shape[0], 1))
-x_class2 = np.column_stack((x_class2, ones_column))
-
-
-# include bias as the 3rd term
-w = np.zeros((3, 1))
-num_examples = x_class1.shape[0] + x_class2.shape[0]
+# gradient descent to train the model
 learning_rate = 0.01
+n_iters = 10000
 prev_loss = None
+for i in range(n_iters):
+    loss, grad = compute_loss_and_gradient(X_train, y_train, weights)
 
-for i in range(1000):
-    y_pred1 = np.dot(x_class1, w)
-    y_pred2 = np.dot(x_class2, w)
-
-    inv_p_pred1 = (1 + np.exp(-y_pred1))
-    p_pred1 = 1 / inv_p_pred1
-    inv_p_pred2 = (1 + np.exp(-y_pred2))
-    p_pred2 = 1 / inv_p_pred2
-
-    # logloss
-    loss = (-1 / num_examples) * (np.sum(1 * np.log(p_pred1)) + np.sum(1 * np.log(1 - p_pred2)))
-
-    if prev_loss is not None:
-        change_perc = (loss - prev_loss) / prev_loss
-        if change_perc < 0.000001:
-            print(f"iter {i}: loss={loss}, prev_loss={prev_loss}, change_perc={change_perc}")
-            break
-
-    grad_class1 = (-1 / num_examples) * (-inv_p_pred1) * p_pred1 * (1 - p_pred1) * x_class1
-    grad_class1 = np.sum(grad_class1, axis=(0))
-    grad_class2 = (-1 / num_examples) * (1 / (1 - p_pred2)) * p_pred2 * (1 - p_pred2) * x_class2
-    grad_class2 = np.sum(grad_class2, axis=(0))
-    grad = grad_class1 + grad_class1
-    grad = grad.reshape(w.shape)
-
-    w -= learning_rate * grad
+    weights -= learning_rate * grad
     if i % 100 == 0:
-        print(f"iter {i}: w={w}")
-    prev_loss = loss
-    
-print(f"final weights (last term is the bias) w={w}")
+        print(f"iter={i}, loss={loss}")
+
+    if prev_loss is None:
+        prev_loss = loss
+    else:
+        if abs((loss - prev_loss) / prev_loss) < 0.0000001:
+            print("loss converges; break")
+            break
+        prev_loss = loss
+# output final model weights
+print(f"final weights weights={weights}")
+
+
+predict(X_test, y_test, weights)
+
+
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+print(model)
+
+# Print the coefficients (weights)
+print("Coefficients:", model.coef_)
+
+# Print the intercept
+print("Intercept:", model.intercept_)
 
 
